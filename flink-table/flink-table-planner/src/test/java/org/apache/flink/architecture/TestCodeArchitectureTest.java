12e47511d2c81564dd0ed69ad83241f3de21f461
Jiabao Sun <jiabao.sun@xtransfer.cn>
2023-11-24 15:57:04 +0800

    Add ArchTest for table-planner-module

--- flink-table/flink-table-planner/src/test/java/org/apache/flink/architecture/TestCodeArchitectureTest.java
+++ flink-table/flink-table-planner/src/test/java/org/apache/flink/architecture/TestCodeArchitectureTest.java
@@ -1,25 +1,28 @@
 package org.apache.flink.architecture;
 
-import com.tngtech.archunit.core.importer.ImportOption;
-import com.tngtech.archunit.junit.AnalyzeClasses;
-
-import com.tngtech.archunit.junit.ArchTest;
-
-import com.tngtech.archunit.junit.ArchTests;
-
 import org.apache.flink.architecture.common.ImportOptions;
 import org.apache.flink.architecture.rules.BanJunit4Rules;
 
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.junit.ArchTests;
+
 /** Architecture tests for test code. */
 @AnalyzeClasses(
-        packages = { "org.apache.flink.table" },
+        packages = {
+                "org.apache.flink.connector",
+                "org.apache.flink.formats",
+                "org.apache.flink.table.api",
+                "org.apache.flink.table.planner",
+                "org.apache.flink.table.test",
+        },
         importOptions = {
-                ImportOption.OnlyIncludeTests.class,
-                ImportOptions.ExcludeScalaImportOption.class,
-                ImportOptions.ExcludeShadedImportOption.class
+            ImportOption.OnlyIncludeTests.class,
+            ImportOptions.ExcludeScalaImportOption.class,
+            ImportOptions.ExcludeShadedImportOption.class
         })
 public class TestCodeArchitectureTest {
 
-    @ArchTest
-    public static final ArchTests BAN_JUNIT4_TESTS = ArchTests.in(BanJunit4Rules.class);
+    @ArchTest public static final ArchTests BAN_JUNIT4_TESTS = ArchTests.in(BanJunit4Rules.class);
 }
